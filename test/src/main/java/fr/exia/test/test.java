fa5c7325eb351ab320b623b5ade496f602d37bfa
ClementFbt <clement.froz@gmail.com>
2017-06-15 08:34:52 +0200

    olilol

--- test/src/main/java/fr/exia/test/test.java
+++ test/src/main/java/fr/exia/test/test.java
@@ -1,8 +1,20 @@
 /*
- * 
+ *
  */
 package fr.exia.test;
 
+import jpu2016.hero.IHero;
+import jpu2016.hit.Hit;
+
 public class test {
-system.out.println("teest");
+
+    public Assassin(final IHero hero) {
+        super(hero);
+    }
+
+    @Override
+    public Hit getHit() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
